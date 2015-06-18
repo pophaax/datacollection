4b6a95fa13221ecb5a396d914bfba8e2dd0b767b
sailbot <sailbot@sailingbot.ax>
2015-06-18 15:42:18 +0300

    removed variables from void dbhandler::getwaypointfromtable()

--- DBHandler.cpp
+++ DBHandler.cpp
@@ -360,14 +360,9 @@ std::vector<std::string> DBHandler::getColumnInfo(std::string info, std::string 
 
 void DBHandler::getWaypointFromTable(WaypointModel &waypointModel){
 
-	std::string id;
-	std::string lat;
-	std::string lon;
-	int radius;
-
 	waypointModel.id = getMinIdFromTable("waypoints");
-	waypointModel.latitude = atof(retriveCell("waypoints", id, "lat").c_str());
-	waypointModel.longitude = atof(retriveCell("waypoints", id, "lon").c_str());
-	waypointModel.radius = retriveCellAsInt("waypoints",id, "radius");
+	waypointModel.latitude = atof(retriveCell("waypoints", waypointModel.id, "lat").c_str());
+	waypointModel.longitude = atof(retriveCell("waypoints", waypointModel.id, "lon").c_str());
+	waypointModel.radius = retriveCellAsInt("waypoints",waypointModel.id, "radius");
 
 }
